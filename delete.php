a557aabec34a8e5f2b2e357d3f2a9efe2e5cf77e
Y. A <96033285+notYourLocalFisherman@users.noreply.github.com>
2021-12-15 00:13:48 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("connect.php");
-if($_GET['id']){
-  $id = $_GET['id'];
-  $sql = "DELETE FROM 'barang' WHERE id ='$id'";
-  $q = mysqli_query($koneksi, $sql);
-  var_dump($q);
-}
- ?>
